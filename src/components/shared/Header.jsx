d8a86a6b66ad81d2cc22ab33eb4911181b2acf38
RazielBlaster <jhojanblackout@gmail.com>
2023-05-11 19:11:46 -0500

    arreglar Cart word

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Cart from '../../pages/cart'
+import Cart from '../../pages/Cart'
 import "./header.css"
 
 const Header = () => {
